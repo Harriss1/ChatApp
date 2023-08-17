ee500e950f0b622df3f8784625d724915d03de18
Klotz <KKlotz@steep.loc>
2023-08-17 11:52:14 +0200

    Projekt Erstellung Netzwerk Prototyp

---
+++ NetworkPrototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkPrototype {
+    internal class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("Insert message to send:");
+            String content = Console.ReadLine();
+            Console.WriteLine("Message:\n\t" + content);
+            Console.ReadKey();
+        }
+    }
+}
