91c265e15c452e546f86a7a34f32e279ecdad51b
Klotz <KKlotz@steep.loc>
2023-08-18 12:20:11 +0200

    Abgabe letzte Korrektur Nachrichten in Konsole

--- NetworkPrototype/Program.cs
+++ NetworkPrototype/Program.cs
@@ -20,12 +20,16 @@ namespace NetworkPrototype {
     internal class Program {
         static void Main(string[] args) {
             // IP-Adressen zur Einrichtung des Clienten oder Servers sowie zum Debuggen
+            System.Console.WriteLine("IP-Adressen des Computers in den Netzwerken:");
             PrintIpAdresses();
 
+
+            System.Console.WriteLine("\nTastendruck-Auswahl Client oder Server:");
             // Auswahl Client/Server
-            System.Console.WriteLine("C - Client with programmatic settings\n" +
+            System.Console.WriteLine(
                 "S - Dynamically setup Server\n" +
-                "D - Dynamically setup Client\n");
+                "D - Dynamically setup Client\n" +
+                "deprecated: C - Client with programmatic settings\n");
             ConsoleKey key = System.Console.ReadKey().Key;
 
             // Connection is set programmatically
@@ -39,7 +43,7 @@ namespace NetworkPrototype {
             // Setup client connection via user input
             if (key == ConsoleKey.D) {
                 System.Console.WriteLine("Client selected\n");
-                System.Console.WriteLine("\tInsert message to send to Hermine:");
+                System.Console.WriteLine("\tInsert message to send:");
                 String content = Console.ReadLine();
 
                 System.Console.WriteLine("\tnumeric IP-Adress:");
@@ -49,24 +53,27 @@ namespace NetworkPrototype {
                 String port = Console.ReadLine();
 
                 Console.WriteLine("Message:\n\t" + content + "\tAdress+Port: " + ipAdress + ":" + port);
+                Console.WriteLine();
+                
                 NetworkClient client = new NetworkClient();
                 client.SendMessageToAdress(ipAdress, port, content);
             }
 
             // Setup server via user input
             if (key == ConsoleKey.S) {
-                System.Console.WriteLine("Server selected\n\n\tIP-Adresse:");
+                System.Console.WriteLine("Server selected\n\n\tnumerische IP-Adresse:");
                 String ipAdress = Console.ReadLine();
                
-                System.Console.WriteLine("\n\tnumeric Port:");
+                System.Console.WriteLine("\nPort:");
                 String port = Console.ReadLine();
-                
+                System.Console.WriteLine();
+
                 NetworkServer server = new NetworkServer();
                 server.StartListening(ipAdress, port);
             }
 
             // Programm Ende
-            Console.WriteLine("Tastendruck beendet Programm.");
+            Console.WriteLine("\nTastendruck beendet Programm.");
             Console.ReadKey();
         }
 
