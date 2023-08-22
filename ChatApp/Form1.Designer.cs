327f0df41d42924b71fb4f1e998ada79db135662
Karl Klotz <karl.klotz@steep.de>
2023-08-22 10:56:44 +0200

    Ordner ChatApp-Projekt Verschoben

---
+++ ChatApp/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ChatApp {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClickThis
+            // 
+            this.buttonClickThis.Location = new System.Drawing.Point(369, 203);
+            this.buttonClickThis.Name = "buttonClickThis";
+            this.buttonClickThis.Size = new System.Drawing.Size(172, 23);
+            this.buttonClickThis.TabIndex = 0;
+            this.buttonClickThis.Text = "Mein erster Button <3";
+            this.buttonClickThis.UseVisualStyleBackColor = true;
+            this.buttonClickThis.Click += new System.EventHandler(this.buttonClickThis_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(165, 80);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(44, 16);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.buttonClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
