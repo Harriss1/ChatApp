c2e908a7293c645a2e10a88d66b3a7378e265c94
Harriss1 <da1men@freenet.de>
2023-09-07 14:22:10 +0200

    Locking durch eine TestKlasse implementiert

---
+++ ThreadedServerPrototype/LockTestObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadedServerPrototype {
+    internal class LockTestObject {
+        private string threadSafeValue;
+
+        // Zeitspanne, welcher der Thread simuliert zu arbeiten
+        private TimeSpan compututationDuration = TimeSpan.FromSeconds(5);
+
+        private LockTestObject() { }
+        public LockTestObject(string threadSafeValue) {
+            this.threadSafeValue = threadSafeValue;
+        }
+
+        public void SetValueWithDelay(string value, string source) {
+            object obj = this;
+            CheckIfLocked(obj, source);
+            lock (obj) {
+                System.Console.WriteLine("warte "+ compututationDuration.TotalSeconds+ " Sekunden für TestLock Änderung[Threadsource={0}]", source);
+                System.Threading.Thread.Sleep(compututationDuration);
+                this.threadSafeValue = value;
+                System.Console.WriteLine("Lock wieder freigegeben [Threadsource={0}]", source);
+            }
+        }
+
+        public string GetValueWithDelay(string source) {
+            object obj = this;
+            CheckIfLocked(obj, source);
+            lock (obj) {
+                System.Console.WriteLine("warte " + compututationDuration.TotalSeconds + " Sekunden für TestLock Anzeige [Threadsource={0}]", source);
+                System.Threading.Thread.Sleep(compututationDuration);
+                Console.WriteLine("Lock wieder freigegeben [Threadsource={0}]", source);
+                return this.threadSafeValue;
+            }
+        }
+
+        private static void CheckIfLocked(object obj, string source) {
+            var lockedBySomeoneElse = !Monitor.TryEnter(obj);
+            if (lockedBySomeoneElse) {
+                System.Console.WriteLine("#### Achtung ####");
+                System.Console.WriteLine("Versuch den geschützten Bereich zu öffnen durch [Threadsource={0}]", source);
+            }
+            else {
+                Monitor.Exit(obj); // Muss den Code erneut freigeben.
+            }
+        }
+
+        public void Print() {
+            System.Console.WriteLine("threadsave text = " + this.threadSafeValue);
+        }
+    }
+}
--- ThreadedServerPrototype/Program.cs
+++ ThreadedServerPrototype/Program.cs
@@ -2,19 +2,46 @@
 using System.Collections.Generic;
 using System.Threading;
 
+/// <summary>
+/// Multithreading Prototyp
+/// Karl Klotz, IA 121, 7.9.2023
+/// 
+/// Zuerst werden zwei Threads gestartet: Server und Test
+/// Danach wird in der Hauptroutine eine Schleife aller 5 Sekunden durchlaufen.
+/// 
+/// Die Hauptroutine gibt somit aller 5 Sekunden den Zustand verschiedener Variablen und Objekte aus.
+/// 
+/// Die Childthreads verändern den Zustand globaler und lokaler Werte in kleineren Zeitabständen kontinuierlich.
+/// 
+/// Ziel des Serverthreads: Änderung globaler und lokaler Thread-Variablen verfolgen
+/// Ziel des Testthreads: Locking eines Codeabschnitts demonstrieren.
+/// 
+/// Das Locking dient zur Sperrung von Objekten und Codeabschnitten, die von mehreren Threads zeitgleich
+/// aufgerufen werden können. Ohne Locking ist die Integrität dieser Daten bei simultanen Zugriff 
+/// nicht gewährleistet.
+/// </summary>
 namespace ThreadedServerPrototype {
     internal class Program {
+        // wird von zwei Threads an unterschiedlichen Stellen bearbeitet
         private static string globalText = "start |";
 
+        // wird von allen drei Threads (Haupt und zwei Child) bearbeitet, es soll aber eine Warnung kommen
+        // falls das Objekt bereits bearbeitet wird.
+        private static LockTestObject lockTestObject = new LockTestObject("startvalue");
+
+        // Nutzung eines Delegate als Funktionspointer
         private delegate string PrintTextCallback(string text);
-        
+
+        private static LocalDataStoreSlot testSlotString = Thread.AllocateNamedDataSlot("testSlotString");
+
         static void Main(string[] args) {
             const string ipAddress = "127.0.0.1";
             const string port = "10015";
 
-            //StartServerThreadObjectivised(ipAddress, port);
             StartServerThreadLambdavised(ipAddress, port);
             int count = 0;
+
+            // purpose of main thread: running the loop every 5 seconds and printing object states
             while (true) {
                 System.Threading.Thread.Sleep(5000);
                 System.Console.WriteLine("still active: " + count++);
@@ -24,14 +51,12 @@ namespace ThreadedServerPrototype {
                 }
                 globalText += "(" + count+")";
                 System.Console.WriteLine("[main] global String =" + globalText);
+               
+                lockTestObject.SetValueWithDelay("main thread altered the locked value", "Main");
+                lockTestObject.Print();
             }
-            //TcpServer tcpServer = new TcpServer(ipAddress, port);
-            //while (true) {
-            //    tcpServer.StartListening();
-            //}
         }
 
-        private static LocalDataStoreSlot testSlotString = Thread.AllocateNamedDataSlot("testSlotString");
         private static void StartServerThreadLambdavised(string ipAddress, string port) {
             PrintTextCallback _printTextCallback = new PrintTextCallback(PrintFancyText);
 
@@ -40,21 +65,14 @@ namespace ThreadedServerPrototype {
             serverHandler.Start();
             
         }
-        // Method that demonstrates working callback
-        public static string PrintFancyText(string text) {
-            // https://csharp-video-tutorials.blogspot.com/2014/03/part-91-retrieving-data-from-thread_13.html
-            System.Console.WriteLine("**** #### Callback Message ### ****");
-            System.Console.WriteLine("\n|\n|\t" + text);
-            return "returned Text=" + text;
-        }
         private static void ServerHandlerThread(string ipAddress, string port, PrintTextCallback _callback) {
+            // Thread.SetDate let us handle individual variables and objects for each thread sperately
             Thread.SetData(testSlotString, "testStringofthisSlot");
             System.Console.WriteLine("Starting server at: " + ipAddress + ":" + port);
             TcpServer server = new TcpServer(ipAddress, port);
             while (true) {
                 System.Console.WriteLine("start loop part");
-                //TcpServerRunOnceAndUnbind tcpServer = new TcpServerRunOnceAndUnbind(ipAddress, port);
-                //tcpServer.StartListening();
+                // code execution stops here and waits for a client request
                 server.Accept();
                 string retrievedString = (string)Thread.GetData(testSlotString);
                 System.Console.WriteLine("serverHandler: retrievedStr=" + retrievedString);
@@ -66,11 +84,21 @@ namespace ThreadedServerPrototype {
                 } else {
                     throw new InvalidOperationException("callbackmethod doesnt work");
                 }
+                lockTestObject.SetValueWithDelay("Thread altered the locked value", "ServerHandler");
+                lockTestObject.Print();
                 System.Console.WriteLine("end loop part");
             }
         }
 
-        // f. object-start
+        // Method that demonstrates working callback
+        public static string PrintFancyText(string text) {
+            // https://csharp-video-tutorials.blogspot.com/2014/03/part-91-retrieving-data-from-thread_13.html
+            System.Console.WriteLine("**** #### Callback Message ### ****");
+            System.Console.WriteLine("\n|\n|\t" + text);
+            return "returned Text=" + text;
+        }
+
+        [Obsolete("deprecated - instanciates and releases a socket object on each call")]
         private static void StartServerThreadObjectivised(string ipAddress, string port) {
             Thread serverHandler = new Thread(ServerHandler);
             List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
@@ -81,6 +109,7 @@ namespace ThreadedServerPrototype {
 
             serverHandler.Start(parameters);
         }
+        [Obsolete("deprecated")]
         private static void ServerHandler(Object parameters) {
             string ipAddress = null;
             string port = null;
